543a4d361455256526eaa5758ecab3e0e929c119
Nikolay <shadowbrocker@gmail.com>
2018-09-10 16:04:27 +0300

    Level 15 Task 1525 - Reading lines from file

---
+++ task1525/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task15.task1525;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Файл в статическом блоке
+*/
+
+public class Solution {
+    public static List<String> lines = new ArrayList<String>();
+
+    static {
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(Statics.FILE_NAME));
+            for (Object line : bufferedReader.lines().toArray()) {
+                lines.add(line.toString());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lines);
+    }
+}
