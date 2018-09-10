dce6247e2a06da223008ce2278c98fffc5b22f15
Nikolay <shadowbrocker@gmail.com>
2018-09-10 15:19:49 +0300

    Level 15 Task 1523 - Constructors overload and various visibility modoficators

---
+++ task1523/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+    public Solution(String s) {}
+    private Solution(int i) {}
+    Solution(double d) {}
+    protected Solution(boolean b) {}
+
+    public static void main(String[] args) {
+
+    }
+}
+
