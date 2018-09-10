9f6ec015985ea79f87e1983f94732e1e96b40866
Nikolay <shadowbrocker@gmail.com>
2018-09-10 15:07:49 +0300

    Level 15 Task 1522 - Singleton lazy initialization

---
+++ task1522/Solution.java
@@ -0,0 +1,45 @@
+package com.javarush.task.task15.task1522;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Закрепляем паттерн Moon
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static Planet thePlanet;
+
+    static {
+        try {
+            readKeyFromConsoleAndInitPlanet();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void readKeyFromConsoleAndInitPlanet() throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String planet = bufferedReader.readLine();
+        switch (planet) {
+            case Planet.EARTH:
+                thePlanet = Earth.getInstance();
+                break;
+            case Planet.MOON:
+                thePlanet = Moon.getInstance();
+                break;
+            case Planet.SUN:
+                thePlanet = Sun.getInstance();
+                break;
+            default:
+                thePlanet = null;
+                break;
+        }
+
+    }
+}
