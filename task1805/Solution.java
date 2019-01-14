8a415de366363d710b31f5cf58c962e5f6b51fea
Nikolay <Shadowborcker@users.noreply.github.com>
2019-01-14 20:29:53 +0300

    Level 18 Task 1805 - Sorting bytes from file using TreeSet

---
+++ task1805/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task18.task1805;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.TreeSet;
+
+/* 
+Сортировка байт
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        List<Integer> readByBytes = new ArrayList<>();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String filename = bufferedReader.readLine();
+        FileInputStream fileInputStream = new FileInputStream(filename);
+
+        while (fileInputStream.available() > 0) {
+            int data = fileInputStream.read();
+            readByBytes.add(data);
+        }
+        bufferedReader.close();
+        fileInputStream.close();
+
+        Collections.sort(readByBytes, Collections.reverseOrder());
+        TreeSet<Integer> treeSet = new TreeSet<>(readByBytes);
+        for (Integer i: treeSet
+             ) {
+            System.out.print(i+ " ");
+        }
+    }
+}
