bcf8d7cd964acfc2862b8d291eef11cae71de4d5
Nikolay <Shadowborcker@users.noreply.github.com>
2019-01-14 15:29:25 +0300

    Level 18 Task 1802 - Finding min byte in file

---
+++ task1802/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task18.task1802;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/* 
+Минимальный байт
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        List<Integer> bytes = new ArrayList<>();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String filename = bufferedReader.readLine();
+        FileInputStream fileInputStream = new FileInputStream(filename);
+
+        while (fileInputStream.available() > 0) {
+            int data = fileInputStream.read();
+            bytes.add(data);
+        }
+        bufferedReader.close();
+        fileInputStream.close();
+        Collections.sort(bytes);
+        System.out.println(bytes.get(0));
+    }
+}
