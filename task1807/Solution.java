1edc5f1c3a09b6735ca57173567a0b8c237fe03b
Nikolay <Shadowborcker@users.noreply.github.com>
2019-01-15 10:19:41 +0300

    Level 18 Task 1807 - Counting number of specific characters in file

---
+++ task1807/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.task18.task1807;
+
+/* 
+Подсчет запятых
+*/
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        List<Integer> bytes = new ArrayList<>();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String filename = bufferedReader.readLine();
+        FileInputStream fileInputStream = new FileInputStream(filename);
+
+        while (fileInputStream.available() > 0) {
+            int data = fileInputStream.read();
+            bytes.add(data);
+        }
+        bufferedReader.close();
+        fileInputStream.close();
+        int count = 0;
+        int c = (int) ',';
+        for (Integer i : bytes) {
+            if (i.equals(c)) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
